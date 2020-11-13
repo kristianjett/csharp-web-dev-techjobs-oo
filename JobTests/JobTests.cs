6091d56535652ef5626d4c9785c8e5ed604c7e1e
Kristian <jettkristian@gmail.com>
2020-11-13 15:11:28 -0800

    Completed unit testing section

--- JobTests/JobTests.cs
+++ JobTests/JobTests.cs
@@ -6,13 +6,15 @@ namespace TechJobTests
     [TestClass]
     public class JobTests
     {
+
         [TestMethod]
         public void TestSettingJobId()
         {
             Job testJob1 = new Job();
             Job testJob2 = new Job();
 
-            Assert.IsFalse(testJob1.Equals(testJob2));
+            Assert.IsFalse(testJob1.Id == testJob2.Id);
+            Assert.AreEqual(testJob1.Id, (testJob2.Id - 1));
         }
 
         [TestMethod]
@@ -22,6 +24,7 @@ namespace TechJobTests
             Location testLocation = new Location("Desert");
             PositionType testPosition = new PositionType("Quality Control");
             CoreCompetency testCompetency = new CoreCompetency("Persistence");
+
             Job testJob3 = new Job("Product Tester", testEmployer, testLocation, testPosition, testCompetency);
 
             Assert.AreEqual("Product Tester", testJob3.Name);
@@ -29,6 +32,21 @@ namespace TechJobTests
             Assert.AreEqual(testLocation, testJob3.EmployerLocation);
             Assert.AreEqual(testPosition, testJob3.JobType);
             Assert.AreEqual(testCompetency, testJob3.JobCoreCompetency);
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Employer testEmployer = new Employer("ACME");
+            Location testLocation = new Location("Desert");
+            PositionType testPosition = new PositionType("Quality Control");
+            CoreCompetency testCompetency = new CoreCompetency("Persistence");
+
+            Job testJob4 = new Job("Product Tester", testEmployer, testLocation, testPosition, testCompetency);
+
+            Job testJob5 = new Job("Product Tester", testEmployer, testLocation, testPosition, testCompetency);
+
+            Assert.IsFalse(testJob4.Equals(testJob5));
         }
     }
 }
